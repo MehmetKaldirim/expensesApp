3f17749bf9f2e18b0bdc742eadf79c48221b9ba7
Mehmet <mathdenizi@gmail.com>
2024-05-27 00:43:59 +0200

    set expenses in context

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { Text } from "react-native";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
@@ -6,18 +6,16 @@ import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 function RecentExpenses() {
-  //const expensesCtx = useContext(ExpensesContext);
-  const [fetchedExpenses, setFetchedExpenses] = useState([]);
+  const expensesCtx = useContext(ExpensesContext);
   useEffect(() => {
     async function getExpenses() {
       const expenses = await fetchExpenses();
-      setFetchedExpenses(expenses);
+      expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
 
-  //const recentExpenses = expensesCtx.expenses.filter((expense) => {
-  const recentExpenses = fetchedExpenses.filter((expense) => {
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
     return expense.date > date7DaysAgo && expense.date <= today;
