fa4010013ba226dd046fd452d586e1c49a5e4348
Mehmet <mathdenizi@gmail.com>
2024-05-27 00:34:52 +0200

    post and fecthed data from backend

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,13 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Text } from "react-native";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  //const expensesCtx = useContext(ExpensesContext);
+  const [fetchedExpenses, setFetchedExpenses] = useState([]);
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+      setFetchedExpenses(expenses);
+    }
+    getExpenses();
+  }, []);
 
-  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  //const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  const recentExpenses = fetchedExpenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
     return expense.date > date7DaysAgo && expense.date <= today;
