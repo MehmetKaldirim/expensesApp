10480c0871cfd2cdf97a90cd996e452c5cd19d92
Mehmet <mathdenizi@gmail.com>
2024-05-27 02:53:01 +0200

    Error Handling added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -11,6 +11,7 @@ import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function ManageExpense({ route, navigation }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState();
   const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
@@ -28,10 +29,14 @@ function ManageExpense({ route, navigation }) {
 
   async function deleteExpenseHandler() {
     setIsSubmitting(true);
-    deleteExpense(editedExpenseId);
-    setIsSubmitting(false);
-    expensesCtx.deleteExpense(editedExpenseId);
-    navigation.goBack();
+    try {
+      deleteExpense(editedExpenseId);
+      expensesCtx.deleteExpense(editedExpenseId);
+      navigation.goBack();
+    } catch (error) {
+      setError("Could not delete expense, please try again later");
+      setIsSubmitting(false);
+    }
   }
 
   function cancelHandler() {
@@ -40,15 +45,25 @@ function ManageExpense({ route, navigation }) {
 
   async function confirmHandler(expenseData) {
     setIsSubmitting(true);
-    if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, expenseData);
-      await updateExpense(editedExpenseId, expenseData);
-    } else {
-      const id = await storeExpense(expenseData);
-      expensesCtx.addExpense({ ...expenseData, id: id });
+    try {
+      if (isEditing) {
+        expensesCtx.updateExpense(editedExpenseId, expenseData);
+        await updateExpense(editedExpenseId, expenseData);
+      } else {
+        const id = await storeExpense(expenseData);
+        expensesCtx.addExpense({ ...expenseData, id: id });
+      }
+      navigation.goBack();
+    } catch (error) {
+      setError("Could not save data- please try again later");
+      setIsSubmitting(false);
     }
-    navigation.goBack();
   }
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} />;
+  }
+
   if (isSubmitting) {
     return <LoadingOverlay />;
   }
